8fbdaa4277ab29d72aa20f5e6b171b0a7ccf52f2
dennisdoolittle123 <112960439+dennisdoolittle123@users.noreply.github.com>
2023-02-13 23:15:10 -0500

    average

--- src/ratings/ProblemSet.java
+++ src/ratings/ProblemSet.java
@@ -1,5 +1,6 @@
 package ratings;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -7,18 +8,35 @@ public class ProblemSet {
 
 
     public static double average(ArrayList<Double> numbers) {
-        return 0.0;
+        double sum = 0.0;
+        for( int i= 0; i < numbers.size(); i +=1){
+            sum+= numbers.get(i);
 
-        // TODO: Implement this method to return the average of all the numbers in the input ArrayList
-        //       If the ArrayList is empty, return 0.0
-        //
-        // Examples
-        // [1.0,2.0,3.0] returns 2.0
-        // [-5.0,5.0] returns 0.0
-        // [6.5,6.5,8.5,8.5] returns 7.5
-        // [] returns 0.0
+
+
+
+
+        }
+        return sum / numbers.size();
+
+
 
     }
+
+
+
+}
+
+
+    // TODO: Implement this method to return the average of all the numbers in the input ArrayList
+    //       If the ArrayList is empty, return 0.0
+    //
+    // Examples
+    // [1.0,2.0,3.0] returns 2.0
+    // [-5.0,5.0] returns 0.0
+    // [6.5,6.5,8.5,8.5] returns 7.5
+    // [] returns 0.0
+
 
 
     // TODO: Write a public static method named sumOfDigits that takes an int as a parameter and
@@ -30,6 +48,7 @@ public class ProblemSet {
     // -36 returns 9
 
 
+
     // TODO: Write a public static method named bestKey that takes a HashMap of String to Integer
     //       as a parameter and returns a key mapping to the largest Integer. Ties can be broken arbitrarily.
     //       If the HashMap is empty, return the empty String
@@ -39,4 +58,3 @@ public class ProblemSet {
     // {"cat": 5, "dog": 5, "fox": 4} can return either "cat" or "dog"
     // {} returns ""
 
-}
